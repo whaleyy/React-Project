195139feccf622a647288be1bd6fff5493316f93
whale <yuan.yang@husky.neu.edu>
2019-09-25 19:52:08 -0400

    add a new react project about id card(even though it's a small project

---
+++ idcard/src/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          date="Today is at 5:00PM"
+          text="that is awesome!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="jack"
+          date="Today is at 9:00AM"
+          text="Great!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="will"
+          date="Today is at 8:00PM"
+          text="I'm gonna be the best developer!"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
