977810cee6c445ddb867dfded2c1369b88466630
whale <yuan.yang@husky.neu.edu>
2019-09-27 16:40:03 -0400

    update

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 // import { super } from "@babel/types";
 
 class App extends React.Component {
@@ -22,7 +23,7 @@ class App extends React.Component {
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude:{this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
     return <div>Loading!</div>;
