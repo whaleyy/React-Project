49296c1ec531e0be97d9c5e51a2236ecebde79cb
whale <yuan.yang@husky.neu.edu>
2019-09-30 10:05:24 -0400

    correct the image problem

--- idcard/src/CommentDetail.js
+++ idcard/src/CommentDetail.js
@@ -4,7 +4,7 @@ const CommentDetail = props => {
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" scr={props.avatar} />
+        <img alt="avatar" src={props.avatar} />
       </a>
       <div className="content">
         <a href="/" className="author">
--- idcard/src/index.js
+++ idcard/src/index.js
@@ -4,35 +4,45 @@ import faker from "faker";
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
 
-const App = () => {
-  return (
-    <div className="ui container comments">
-      <ApprovalCard>
-        <CommentDetail
-          author="Sam"
-          date="Today is at 5:00PM"
-          text="that is awesome!"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="jack"
-          date="Today is at 9:00AM"
-          text="Great!"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-      <ApprovalCard>
-        <CommentDetail
-          author="will"
-          date="Today is at 8:00PM"
-          text="I'm gonna be the best developer!"
-          avatar={faker.image.avatar()}
-        />
-      </ApprovalCard>
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { avatarA: "" };
+
+  componentDidMount() {
+    this.setState({ avatarA: faker.image.avatar() });
+    // const avatarB = faker.image.avatar();
+    // const avatarC = faker.image.avatar();
+  }
+
+  render() {
+    return (
+      <div className="ui container comments">
+        <ApprovalCard>
+          <CommentDetail
+            author="Sam"
+            date="Today is at 5:00PM"
+            text="that is awesome!"
+            avatar={this.state.avatarA}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="jack"
+            date="Today is at 9:00AM"
+            text="Great!"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+        <ApprovalCard>
+          <CommentDetail
+            author="will"
+            date="Today is at 8:00PM"
+            text="I'm gonna be the best developer!"
+            avatar={faker.image.avatar()}
+          />
+        </ApprovalCard>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
